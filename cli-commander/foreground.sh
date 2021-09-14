144363d96cb835100a525c3ed0d82752596f69c9
Maksim Ustinov <maksimu@gmail.com>
2021-09-14 12:15:40 -0700

    f

--- cli-commander/foreground.sh
+++ cli-commander/foreground.sh
@@ -3,9 +3,9 @@
 echo -e "\e[101;93mDO NOT USE YOUR PRODUCTION CREDENTIALS IN ANY OF THESE EXAMPLES\e[0m"
 
 while true; do
-    read -p "Type 'Y' to acknowledge:?" yn
+    read -p "Type 'Y' to acknowledge: " yn
     case $yn in
         [Yy]* ) break;;
-        * ) echo "Please answer yes by typing Y to acknowledge.";;
+        * ) echo "Please answer yes by typing Y to acknowledge. ";;
     esac
 done
