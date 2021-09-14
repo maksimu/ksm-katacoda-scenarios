5050c5e0f4e3810e597f5ed7ab2f361d0d739c54
Maksim Ustinov <maksimu@gmail.com>
2021-09-14 12:07:34 -0700

    f

--- cli-commander/foreground.sh
+++ cli-commander/foreground.sh
@@ -1,5 +1,5 @@
 # python --version
 # pip3 list
-echo -e "\e[101;93m_______________________________________________________________"
-echo -e "\e[101;93mDO NOT USE YOUR PRODUCTION CREDENTIALS IN ANY OF THESE EXAMPLES"
-echo -e "\e[101;93m_______________________________________________________________"
+echo -e "\e[101;93m---------------------------------------------------------------\e[0m"
+echo -e "\e[101;93mDO NOT USE YOUR PRODUCTION CREDENTIALS IN ANY OF THESE EXAMPLES\e[0m"
+echo -e "\e[101;93m---------------------------------------------------------------\e[0m"
