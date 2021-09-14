c4be475814c9a8fbd3827ed2108b5b4e6a99ec13
Maksim Ustinov <maksimu@gmail.com>
2021-09-14 12:13:28 -0700

    df

--- cli-commander/foreground.sh
+++ cli-commander/foreground.sh
@@ -1,5 +1,11 @@
 # python --version
 # pip3 list
-echo -e "\e[101;93m---------------------------------------------------------------\e[0m"
 echo -e "\e[101;93mDO NOT USE YOUR PRODUCTION CREDENTIALS IN ANY OF THESE EXAMPLES\e[0m"
-echo -e "\e[101;93m---------------------------------------------------------------\e[0m"
+
+while true; do
+    read -p "Type 'Y' to acknowledge:?" yn
+    case $yn in
+        [Yy]* ) break;;
+        * ) echo "Please answer yes by typing Y to acknowledge.";;
+    esac
+done
