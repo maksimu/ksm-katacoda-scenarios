59cf364442f352ff899eb09a99bcb239f957f3c8
Maksim Ustinov <maksimu@gmail.com>
2021-09-22 21:41:55 -0700

    e

--- terraform/install-terraform-ubuntu.sh
+++ terraform/install-terraform-ubuntu.sh
@@ -5,11 +5,7 @@
 # Source: https://learn.hashicorp.com/tutorials/terraform/install-cli
 
 sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
-
-# Add the HashiCorp GPG key.
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-
-# Add the official HashiCorp Linux repository.
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 
 # Update to add the repository, and install the Terraform CLI.
