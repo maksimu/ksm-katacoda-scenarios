e411f05d82c2fd5aeeb4e4b6b8741e1029a9c5d5
Maksim Ustinov <maksimu@gmail.com>
2021-09-22 23:12:03 -0700

    h

--- terraform/install-terraform-ubuntu.sh
+++ terraform/install-terraform-ubuntu.sh
@@ -4,17 +4,17 @@
 # Install Terraform CLI in Ubuntu
 # Source: https://learn.hashicorp.com/tutorials/terraform/install-cli
 echo "DDD"
-apt-get update
+apt update
 # apt-get install -y curl
 # apt-get install -y gnupg 
-apt-get install -y software-properties-common
+apt install -y software-properties-common
 
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 
 # Update to add the repository, and install the Terraform CLI.
-apt-get update -y
-apt-get install -y terraform
+apt update -y
+apt install -y terraform
 
 # Enable tab completion
 touch ~/.bashrc
