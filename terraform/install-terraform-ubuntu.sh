3bcb0fa1e00adc8298cea43ff859e580daf8026e
Maksim Ustinov <maksimu@gmail.com>
2021-09-22 21:42:48 -0700

    d

--- terraform/install-terraform-ubuntu.sh
+++ terraform/install-terraform-ubuntu.sh
@@ -3,7 +3,7 @@
 
 # Install Terraform CLI in Ubuntu
 # Source: https://learn.hashicorp.com/tutorials/terraform/install-cli
-
+echo "DDD"
 sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
